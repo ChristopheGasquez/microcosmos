d43703fe0374ba22de4198f9d82b28fff6e59d8a
gasquez.christophe <c.gasquez.dl@gmail.com>
2025-11-23 15:33:17 +0100

    Improvement - Removed Renderer2 from title component.

--- projects/microcosmos-lib/src/lib/components/title/title.component.ts
+++ projects/microcosmos-lib/src/lib/components/title/title.component.ts
@@ -1,45 +1,47 @@
-import { Component, ElementRef, HostBinding, inject, Input, type OnInit, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostBinding, inject, Input } from '@angular/core';
 import { COLORS, type Colors, TEXT_ALIGN, type TextAlign, WEIGHT, type Weight } from '../../constants';
 
 @Component({
   standalone: true,
   selector: 'mcs-h1, mcs-h2, mcs-h3, mcs-h4, mcs-h5, mcs-h6',
-  template: '<ng-content></ng-content>',
+  imports: [ CommonModule ],
+  template: `
+    @switch (level) {
+      @case (1) { <h1><ng-container [ngTemplateOutlet]="content"></ng-container></h1> }
+      @case (2) { <h2><ng-container [ngTemplateOutlet]="content"></ng-container></h2> }
+      @case (3) { <h3><ng-container [ngTemplateOutlet]="content"></ng-container></h3> }
+      @case (4) { <h4><ng-container [ngTemplateOutlet]="content"></ng-container></h4> }
+      @case (5) { <h5><ng-container [ngTemplateOutlet]="content"></ng-container></h5> }
+      @case (6) { <h6><ng-container [ngTemplateOutlet]="content"></ng-container></h6> }
+    }
+
+    <ng-template #content>
+      <ng-content></ng-content>
+    </ng-template>
+  `,
   styleUrls: [ './title.component.scss' ],
-  host: {
-    'class': 'mcs-title',
-  },
+  host: { 'class': `mcs-title` },
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent {
   readonly #host: ElementRef<HTMLElement> = inject(ElementRef<HTMLElement>);
-  readonly #renderer: Renderer2 = inject(Renderer2);
 
-  public level: string = 'h1';
+  @Input() color: Colors = COLORS.INK;
+  @Input() weight: Weight = WEIGHT.REGULAR;
+  @Input() ellipsis: boolean = true;
+  @Input() align: TextAlign = TEXT_ALIGN.START;
 
-  @Input() public color: Colors = COLORS.INK;
-  @Input() public weight: Weight = WEIGHT.REGULAR;
-  @Input() public ellipsis: boolean = true;
-  @Input() public align: TextAlign = TEXT_ALIGN.START;
+  level: number = Number(
+    this.#host.nativeElement.tagName.toLowerCase().replace('mcs-h', ''),
+  );
 
   @HostBinding('class') get hostClasses(): string {
     return `
-      mcs-title--${ this.level }
+      mcs-title--h${ this.level }
       mcs-color--${ this.color }
       mcs-ellipsis--${ this.ellipsis ? 'true' : 'false' }
       mcs-text-align--${ this.align }
       mcs-weight--${ this.weight }
-    `.trim().replace(/\s+/g, ' ');
-  }
-
-  ngOnInit() {
-    const tag: string = this.#host.nativeElement.tagName.toLowerCase();
-    this.level = tag.replace('mcs-', '');
-
-    const heading = this.#renderer.createElement(this.level);
-
-    while (this.#host.nativeElement.firstChild) {
-      this.#renderer.appendChild(heading, this.#host.nativeElement.firstChild);
-    }
-    this.#renderer.appendChild(this.#host.nativeElement, heading);
-  }
+    `.trim();
+  };
 }
